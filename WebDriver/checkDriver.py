df04b5adaeb4757ab002ad53cc9064d954390d79
Locullus <bula.mickael@gmail.com>
2021-08-28 23:08:08 +0200

    add a session in webdriver

--- WebDriver/checkDriver.py
+++ WebDriver/checkDriver.py
@@ -11,8 +11,7 @@ from tools_functions import check_isfile
 check_isfile('chromedriver.exe')
 
 # on lance une session
-url = "http://example.org/"
-test = Webdriver(url)
+driver = Webdriver("http://www.python.org")
 
 # TODO : sur cette base on va pouvoir intégrer le module database pour ensuite lancer des sessions sur Boursorama
 # TODO : on intégrera tout ceci dans une application flask avec une interface graphique correcte
--- WebDriver/classWebdriver.py
+++ WebDriver/classWebdriver.py
@@ -1,5 +1,6 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import SessionNotCreatedException
 import time
 
@@ -16,6 +17,13 @@ class Webdriver:
                 self.driver.implicitly_wait(2)
                 self.driver.maximize_window()
                 self.driver.get(self.url)
+                assert "Python" in self.driver.title
+                elem = self.driver.find_element_by_name("q")
+                elem.clear()
+                elem.send_keys("pycon")
+                elem.send_keys(Keys.RETURN)
+                assert "No results found." not in self.driver.page_source
                 time.sleep(10)
+                self.driver.close()
         except SessionNotCreatedException:
             print("problème avec la version actuelle du chromedriver...")
